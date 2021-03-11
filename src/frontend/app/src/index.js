c7fa7ed2f40cfcdaa1f28ea87c3714ad8125df99
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-03-10 17:35:00 -0800

    Add React app and modify build to support it

---
+++ src/frontend/app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.print = this.print.bind(this)
+    }
+    print(){
+        window.Module.ready.then(api => console.log(api.print(100)))
+    }
+
+    render(){
+        return (
+            <button onClick={this.print}>Print 100</button>)
+    }
+}
+ReactDOM.render(<App/>, document.getElementById("root"))
