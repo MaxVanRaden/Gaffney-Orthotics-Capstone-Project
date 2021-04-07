9aa773fc6b93ffa115534bc8ff67fe5b80c11009
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-04-07 14:19:02 -0700

    Add ExortFile component to react app
    Creates ExportFile.js, imports it in App.js, and updates wrapper.js to include the current export_model function.

---
+++ frontend/app/src/components/ExportFile.js
@@ -0,0 +1,51 @@
+import {saveAs} from 'file-saver'
+import {useState} from 'react'
+
+const Export = () => {
+    const [format, setFormat] = useState('.obj')
+
+    const handleChange = (e) => {setFormat(e.target.value)}
+
+    const handleClick = () => {
+        window.Module.ready.then(api => {
+            /*
+            Potential process to get data
+            api.store_model(format)
+            api.get_length()
+            api.export_model(id)
+            */
+            //TODO:get actual length of byte array
+            var len = 5;
+
+            //get pointer to byte array
+            var addr = api.export_model(0, format)
+
+            //read array from wasm heap
+            const data = new Uint8Array(window.Module.HEAP8.subarray(addr, addr+len))
+
+            //save data with selected file format
+            saveAs(new Blob([data]),'file'+format)
+        })
+    }
+    return (
+        <div>
+            <input type="radio"
+                   onChange={handleChange}
+                   value=".stl"
+                   checked={format === '.stl'}/>.stl
+            <input type="radio"
+                   onChange={handleChange}
+                   value=".obj"
+                   checked={format === '.obj'}/>.obj
+            <button onClick={handleClick}>Export</button>
+        </div>
+    )
+}
+
+export const ExportFile = () => {
+    return (
+        <div>
+            <Export />
+        </div>
+    )
+}
