0574d7a5f1877ef29ac484980616efac9ac7c324
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-04-07 19:15:29 -0700

    Update ExportFile.js

--- frontend/app/src/components/ExportFile.js
+++ frontend/app/src/components/ExportFile.js
@@ -2,31 +2,31 @@ import {saveAs} from 'file-saver'
 import {useState} from 'react'
 
 const Export = () => {
-    const [format, setFormat] = useState('.obj')
+    const [format, setFormat] = useState('.obj');
 
-    const handleChange = (e) => {setFormat(e.target.value)}
+    const handleChange = (e) => {setFormat(e.target.value);}
 
     const handleClick = () => {
         window.Module.ready.then(api => {
             /*
             Potential process to get data
             api.store_model(format)
-            api.get_length()
+            api.get_length(id)
             api.export_model(id)
             */
             //TODO:get actual length of byte array
             var len = 5;
 
             //get pointer to byte array
-            var addr = api.export_model(0, format)
+            var addr = api.export_model(0, format);
 
             //read array from wasm heap
-            const data = new Uint8Array(window.Module.HEAP8.subarray(addr, addr+len))
+            const data = new Uint8Array(window.Module.HEAP8.subarray(addr, addr+len));
 
             //save data with selected file format
-            saveAs(new Blob([data]),'file'+format)
+            saveAs(new Blob([data]),'file'+format);
         })
-            .catch(e => console.log("Failed to export model"))
+            .catch(e => console.log("Failed to export model"));
     }
     return (
         <div>
@@ -40,7 +40,7 @@ const Export = () => {
                    checked={format === '.obj'}/>.obj
             <button onClick={handleClick}>Export</button>
         </div>
-    )
+    );
 }
 
 export const ExportFile = () => {
@@ -48,5 +48,5 @@ export const ExportFile = () => {
         <div>
             <Export />
         </div>
-    )
+    );
 }
