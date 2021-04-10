42abde446966776484771787229e3d5622e3a153
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-04-09 21:28:30 -0700

    Update Rotate.js

--- frontend/app/src/components/Rotate.js
+++ frontend/app/src/components/Rotate.js
@@ -1,8 +1,8 @@
 export const Rotate = () => {
     return (
         <div className='rotate'>
-            <input type='checkbox' id='rtoggle' className='rtoggle'/>
-                <label htmlFor='rtoggle' id='rlabel'>Rotate</label>
+            <input type='checkbox' id='rtoggle2' className='rtoggle'/>
+                <label htmlFor='rtoggle2' id='rlabel'>Rotate</label>
         </div>
     )
 }
