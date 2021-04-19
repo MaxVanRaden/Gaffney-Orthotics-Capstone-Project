d18b2aa5b12b4cd93f2db3ab5d010f6055aa59b1
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-04-19 15:11:37 -0700

    Add wrapper for export_new

--- frontend/wrapper.js
+++ frontend/wrapper.js
@@ -5,6 +5,7 @@ Module.ready = new Promise(function(resolve, reject) {
         var api = {
             import_model: Module.cwrap('import_model', null, ['number','number']),
             export_model: Module.cwrap('export_model', 'number', ['number','number']),
+            export_new: Module.cwrap('export_new', 'number',['number','string']),
             on_mouse_up: Module.cwrap('on_mouse_up', null, ['number']),
             get_export_strlen: Module.cwrap('get_export_strlen', 'number', ['number']),
             scale: Module.cwrap('scale',null,['number'])
