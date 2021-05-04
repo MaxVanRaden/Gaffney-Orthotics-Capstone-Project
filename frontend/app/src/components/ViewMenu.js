d815af1da430e0985388cf38bc787baf15ec0518
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-05-03 18:21:11 -0700

    Set window size automatically

--- frontend/app/src/components/ViewMenu.js
+++ frontend/app/src/components/ViewMenu.js
@@ -1,28 +1,8 @@
 /* eslint-disable */
-import {useState} from "react";
 
 export const ViewMenu = (props) => {
-    const [width, setWidth] = useState(1507)
-    const [height, setHeight] = useState(800)
-    const canvas = document.getElementById("canvas");
-    //Change canvas size
-    const handleSize = (e) => {
-        let val = Number(e.target.value);
-        switch(e.target.className){
-            case "width":
-                setWidth(val);
-                window.Module._set_size(val, height);
-                canvas.style.width = String(val) + 'px';
-                break;
-            case "height":
-                setHeight(val);
-                window.Module._set_size(width, val);
-                canvas.style.height = String(val) + 'px';
-                break;
-            default:
-                console.log("Invalid classname")
-        }
-    }
+
+
     return (
         <div className="dropdown">
             <button className="tool">View</button>
@@ -39,16 +19,6 @@ export const ViewMenu = (props) => {
                 </div>
                 <a href="#">Perspective</a>
                 <a onClick={() => document.getElementById('fullscreen').click()}>Fullscreen</a>
-                <div>
-                    Width
-                    <input type="number" className="width" style={{marginLeft:5}}
-                           min="1" max="2000"
-                           value={width} onChange={handleSize}/>
-                    Height
-                    <input type="number" className="height" style={{marginLeft:5, maxWidth:60}}
-                           min="1" max="2000"
-                           value={height} onChange={handleSize}/>
-                </div>
             </div>
         </div>
     )
