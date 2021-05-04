5ae32b58e88e5d23b079b1744d9fe209c3abc74c
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-05-03 18:35:46 -0700

    Add onresize listener

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -8,6 +8,12 @@ function App() {
         document.getElementById("root").onwheel = function(e){
             e.preventDefault()
         }
+        window.onresize = function(e){
+            window.Module._set_size(window.innerWidth, window.innerHeight)
+            let canvas = document.getElementById("canvas")
+            canvas.style.width = window.innerWidth + 'px'
+            canvas.style.height = window.innerHeight + 'px'
+        }
     },[])
 
     return (
