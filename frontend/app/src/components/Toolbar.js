8d771ce3c66147df14cb126dcc3e91ef764dd624
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2021-05-07 21:35:41 -0700

    Add button to toggle dev_stuff

--- frontend/app/src/components/Toolbar.js
+++ frontend/app/src/components/Toolbar.js
@@ -6,16 +6,24 @@ import {EditMenu} from "./EditMenu";
 import {SelectMenu} from "./SelectMenu";
 import {Coords} from "./Coords";
 import {Camera} from "./Camera";
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {ToolGroup} from "./ToolGroup";
 
 export const Toolbar = () => {
     const [tool, setTool] = useState('select');
     const [zoom, setZoom] = useState(0);
+    const [dev, setDev] = useState(false)
+    useEffect(() =>{
+        let devStuff = document.getElementById("dev_stuff")
+        let body = document.getElementsByClassName("main")[0]
+        devStuff.style.display = dev ? "block" : "none";
+        body.style.overflow = dev ? "visible" : "hidden";
+    },[dev])
     return(
         <div id ="toolbar" className="toolbar">
             <a href="./" className="backbtn">&#8617;</a>
             <a href="./" className="backbtn">&#8618;</a>
+            <button id="devToggle" className="tool" onClick={() => setDev(!dev)}>Dev Stuff</button>
             <ImportFile/>
             <ExportFile/>
             <TranslateVertex/>
